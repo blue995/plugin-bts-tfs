759081bc64202201908e095e1fc2762a32952c48
Tobias Blaufuß <t.blaufuss@gmx.de>
2020-01-27 23:45:39 +0100

    [UPDATE] init-repo script

--- repo-management/init-repo.bash
+++ repo-management/init-repo.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+CURRENT_DIR=.
+BASEDIR=$(dirname "$0")
+
+cd "$BASEDIR"/..
 git remote add main https://github.com/reportportal/plugin-bts-rally.git
 git fetch main
 git checkout -t main/master
 git checkout rally-transformed-to-tfs
+cd "$CURRENT_DIR"
