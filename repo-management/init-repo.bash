716a63ff10d3743dd4405bbbece1527348bb45d7
Tobias Blaufuß <tobias.blaufuss@siemens-healthineers.com>
2020-01-28 15:25:28 +0100

    [FIX] init repo script

--- repo-management/init-repo.bash
+++ repo-management/init-repo.bash
@@ -8,6 +8,6 @@ git remote rename origin patched
 git remote add origin https://github.com/reportportal/plugin-bts-rally.git
 git fetch origin
 git branch master
-git branch master --set-upstream-to origin
+git branch master --set-upstream-to origin/master
 git checkout rally-transformed-to-tfs
 cd "$CURRENT_DIR"
