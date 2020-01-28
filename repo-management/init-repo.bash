546c1dee1fed2aadfed33279d7db9b8365f97ff8
Tobias Blaufuß <tobias.blaufuss@siemens-healthineers.com>
2020-01-28 15:16:50 +0100

    [UPDATE] Repo script withour changing branches

--- repo-management/init-repo.bash
+++ repo-management/init-repo.bash
@@ -4,8 +4,10 @@ CURRENT_DIR=$PWD
 BASEDIR=$(dirname "$0")
 
 cd "$BASEDIR"/..
-git remote add main https://github.com/reportportal/plugin-bts-rally.git
-git fetch main
-git checkout -t main/master
+git remote rename origin patched
+git remote add origin https://github.com/reportportal/plugin-bts-rally.git
+git fetch origin
+git branch master
+git branch master --set-upstream-to origin
 git checkout rally-transformed-to-tfs
 cd "$CURRENT_DIR"
